e009236764dd71bd8f2155194975f9f32dcc02b5
26th_Official <26thofficial.creator@gmail.com>
2022-10-25 15:56:20 +0530

    Final

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 function rotatewheel(){
     let b = document.querySelector(".wheel").style.rotate
-    console.log(b)
     let a = Math.random()
-    console.log(a)
-    let v = (a *360)+360
-    console.log(v)
+    let v = (a *360)+600
     if (b.slice(0,2)<-1){
         document.querySelector(".wheel").style.rotate= v+"deg"
-        b = false
     }
     else{
         document.querySelector(".wheel").style.rotate= -v+"deg"
-        b = true
-    }
-    
+    } 
 }
 
 document.addEventListener('keydown', (event) => {
-    var name = event.key;
-    if (name == "Enter"){
+    if (event.key == "Enter"){
         rotatewheel()
     }
 })
